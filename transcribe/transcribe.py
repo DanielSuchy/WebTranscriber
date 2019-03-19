2beee8a369bd0d7e283916a143b1d4a04d07269c
DanielSuchy <daniel.suchy@gmail.com>
2019-03-19 21:57:15 +0100

    jednoduché kopírování ze zdroje do cílu

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -6,4 +6,13 @@ bp = Blueprint('transcribe', __name__, url_prefix='/transcribe')
 
 @bp.route('/', methods=('GET', 'POST'))
 def presenters():
-  return render_template('transcribe/transcribe.html')
+  source_text = "vložte text pro transkripci do IPA"
+  target_text = "zde se zobrazí přeložený text"
+  if request.method == 'POST':
+      source_text = request.form['source_text']
+      target_text = source_text
+
+  return render_template('transcribe/transcribe.html',
+                          source_text = source_text,
+                          target_text = target_text
+                        )
